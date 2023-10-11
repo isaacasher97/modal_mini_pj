d5bed369b9c36d1c26f82bf0f6bb3b419656eff2
isaacasher97 <isaac.asher97@gmail.com>
2023-10-11 15:18:15 -0400

    simple modal made in vueJs

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <Modal/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      title: 'Final Modal Title'
+    }
+  },
+  components: { Modal }
 }
 </script>
 
